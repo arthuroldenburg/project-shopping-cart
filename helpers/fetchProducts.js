9c63f294190857fd369f696d07430aa94f87de1f
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2022-10-11 16:06:51 -0300

    Requisito 1 feito

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,16 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (productName) => {
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const result = await fetch(url);
+  const data = await result.json();
+  return data;
 };
+
+const requestComputer = async () => {
+  const data = await fetchProducts('computador');
+  return data;
+};
+
+requestComputer();
 
 if (typeof module !== 'undefined') {
   module.exports = {
