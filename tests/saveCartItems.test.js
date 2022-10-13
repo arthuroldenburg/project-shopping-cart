2d98b5e3635bbf8a9c864be8aa1d8f304946f730
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2022-10-13 17:01:39 -0300

    Requisito 2 feito

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,9 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', () => {
+
+  })
 });
+
+// fail('Teste vazio');
